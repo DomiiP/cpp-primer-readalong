365bf74cb3b79050094dffd82ec4ab51afcf0539
DomiiP <95188800+DomiiP@users.noreply.github.com>
2022-09-07 23:32:55 +0200

    Update main.cpp
    Changed r2 to M_PI for PI constant :)

--- ch02/ex16/main.cpp
+++ ch02/ex16/main.cpp
@@ -1,16 +1,17 @@
+#define _USE_MATH_DEFINES
+#include <math.h>
+#include <iostream>
 // Which, If any, of the following assignments are invalid?
 // if they are valid, explain what they do
-#include <iostream>
 
 int main (void)
 {
     int i = 0, &r1 = i;
     double d = 0, &r2 = d;
     std::cout << "int i = " << i << "&r1 = " << &r1 << std::endl;
-    r2 = 3.14159;
-    std::cout << "r2 = " << r2 << std::endl;
+    std::cout << "r2 = " << M_PI << std::endl;
     std::cout << "d = " << d << std::endl;
-    i = r2;
+    i = M_PI;
     std::cout << "i = " << i << std::endl;
     r2 = r1;
     std::cout << "r2 = " << r2 << std::endl;
